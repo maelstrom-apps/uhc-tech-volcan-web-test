e04ea584a9fc436c6ca5c553e3dcf788641feae4
jason-wilson_uhg <jason-wilson_uhg@users.noreply.github.com>
2022-12-09 19:45:03 +0000

    deploy: uhc-tech/volcan@91dc66341e081464ac2e684abe55437858c56146

---
+++ _next/static/KJxEGtBuq67rYk5XDLDU9/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[{source:"/:path*",destination:"/"}],fallback:[]},"/":["static/chunks/362-38b4a6259502e862.js","static/chunks/pages/index-244df35486d06a81.js"],"/_error":["static/chunks/pages/_error-a1d5bc3e43249d4c.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
